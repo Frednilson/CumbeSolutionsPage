f80f73bb2c158aa9d1288d69ed0e29770a47a8b2
Frednilson <140554156+Frednilson@users.noreply.github.com>
2026-05-12 14:43:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
